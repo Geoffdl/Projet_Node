b3873fc525c5fa0657450dba905e8b3f78c197dc
Geoffdl <geoffroydelorme@gmail.com>
2025-03-11 14:08:20 +0100

    c'est parti la roquette

--- model/bars.js
+++ model/bars.js
@@ -11,7 +11,7 @@ const sequelize = require("sequelize");
 
 const Bar = db.define("bars", {
     id: { type: sequelize.INTEGER, autoIcrement: true, primaryKey: true },
-    nom: { type: sequelize.STRING, allowNull: false },
+    nom: { type: sequelize.STRING, unique: true, allowNull: false },
     adresse: { type: sequelize.STRING, allowNull: false },
     tel: { type: sequelize.STRING, allowNull: true },
     email: { type: sequelize.STRING, allowNull: false },
--- router/routers.js
+++ router/routers.js
@@ -2,5 +2,6 @@ const express = require("express");
 const barRouter = require("./barRouter");
 const biereRouter = require("./biereRouter");
 // const commandeRouter = require("./commandeRouter");
+// const biere_commandeRouter = require("./biere_commandeRouter");
 
 module.exports = { barRouter, biereRouter };
