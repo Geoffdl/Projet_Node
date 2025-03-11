bdedb8e555edda41005cc9b6e5f81de613c2b248
Geoffdl <geoffroydelorme@gmail.com>
2025-03-11 14:56:32 +0100

    +commande fonctionnality in index

--- router/routers.js
+++ router/routers.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const barRouter = require("./barRouter");
 const biereRouter = require("./biereRouter");
-// const commandeRouter = require("./commandeRouter");
+const commandeRouter = require("./commandeRouter");
 // const biere_commandeRouter = require("./biere_commandeRouter");
 
-module.exports = { barRouter, biereRouter };
+module.exports = { barRouter, biereRouter, commandeRouter };
