9829940cb400d89f592f5f3446164f275f239801
Emikoya <pruvost.lily@gmail.com>
2025-03-12 16:32:01 +0100

    Merge pull 2

--- router/endpointAvance_1Router.js
+++ router/endpointAvance_1Router.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-<<<<<<< HEAD
 const { listBiereByAlcool,
     listCommandByQueries } = require("../controller/endpointAvance_1Controller");
 
@@ -8,22 +7,5 @@ const router = express.Router();
 
 router.get("/bars/:id/degree", listBiereByAlcool);
 router.get("/bars/:id/commandes", listCommandByQueries)
-=======
-const {
-    listBiereByAlcoolDegreeBetweenPrize,
-    listBiereByAlcoolDegreeInADate,
-    listCommandByDateBetweenPrize,
-    listCommandByDateBetweenPrizeWhenFinish,
-    listCommandByDateBetweenPrizeWhenFinishWithName,
-} = require("../controller/endpointAvance_1Controller");
-
-const router = express.Router();
-
-router.get("/bars/:id/degree?prix_min&prix_max", listBiereByAlcoolDegreeBetweenPrize);
-router.get("/bars/:id/degree?date", listBiereByAlcoolDegreeInADate);
-router.get("/bars/:id/commandes?date&prix_min&prix_max", listCommandByDateBetweenPrize);
-router.get("/bars/:id/commandes?date&prix_min&prix_max&status", listCommandByDateBetweenPrizeWhenFinish);
-router.get("/bars/:id/commandes?date&prix_min&prix_max&status&name", listCommandByDateBetweenPrizeWhenFinishWithName);
->>>>>>> f95daed1bfc3c2f3ce991623513531abb740261b
 
 module.exports = router;
