f8aa93603aeab1a99473f5e89f1683635eb44ad5
Geoffdl <geoffroydelorme@gmail.com>
2025-03-12 10:26:08 +0100

    correction des routes pour lancer l'api

--- router/biereRouter.js
+++ router/biereRouter.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { update, destroy, show, addBiereBar, getBiereBar, getAvgBiereBar } = require("../controller/biereController");
-const BiereValidator = require("../middleware/formRequest/BiereFormRequest");
+const { validateBiere } = require("../middleware/formRequest/BiereFormRequest");
 
 const router = express.Router();
 
 router.get("/biere/:id", show);
-router.put("/biere/:id", BiereValidator, update);
+router.put("/biere/:id", validateBiere, update);
 router.delete("/biere/:id", destroy);
 
-router.post("/bars/:id_bar/biere", BiereValidator, addBiereBar);
+router.post("/bars/:id_bar/biere", validateBiere, addBiereBar);
 router.get("/bars/:id_bar/biere", getBiereBar);
 
 router.get("/bars/:id_bar/degree", getAvgBiereBar);
