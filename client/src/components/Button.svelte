12572dec76e421af2b339322fd7ad3fb37e57fef
Emikoya <pruvost.lily@gmail.com>
2025-03-17 10:16:09 +0100

    correction du boutton annuler

--- client/src/components/Button.svelte
+++ client/src/components/Button.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     export let title = '';
     export let onclick = () => {};
-    let buttonColor = title === '+ Ajouter' || 'Valider' || 'Annuler' || 'Manager ce bar' || 'Bières' || 'Commandes'
+    let buttonColor = ['+ Ajouter', 'Valider', 'Annuler', 'Manager ce bar', 'Bières', 'Commandes'].includes(title)
         ? 'bg-[#EAD982] hover:bg-[#B1A879]' 
         : title === "Supprimer"
             ? 'bg-[#E43347] hover: bg-[#99333F]'
             : 'bg-stone-50 hover: bg-stone-150'
+    let buttonType: 'button' | 'submit' | 'reset' = title === 'Annuler' 
+    ? 'button' 
+    : 'submit'
 
     // Button style
     const styles = {
@@ -13,4 +16,4 @@
     };
 </script>
 
-<button class={styles.button} on:click={onclick}>{title}</button>
+<button type = {buttonType} class={styles.button} on:click={onclick}>{title}</button>
