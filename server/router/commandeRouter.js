64e74ddd8fe1c171e214d3e81c2259a02433a800
Emikoya <pruvost.lily@gmail.com>
2025-03-17 12:13:10 +0100

    mise en place controller + router liste commandes

--- server/router/commandeRouter.js
+++ server/router/commandeRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { index, show, store, update, destroy, getCommandPDF } = require("../controller/commandeController");
+const { index, show, store, update, destroy, allCommande, getCommandPDF } = require("../controller/commandeController");
 const { validateCommande, updateCommande } = require("../middleware/formRequest/commande_formControl");
 
 const router = express.Router();
@@ -10,5 +10,7 @@ router.get("/commande/details/:id_commande", getCommandPDF);
 router.post("/bars/:id_bar/commandes", validateCommande, store);
 router.put("/commandes/:id_commande", updateCommande, update);
 router.delete("/commandes/:id_commande", destroy);
+
+router.get('/commandes', allCommande)
 
 module.exports = router;
